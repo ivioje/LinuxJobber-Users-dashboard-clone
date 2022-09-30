db03aed9249dbe811bee4a92016085221a36f6a9
Eboreime Rhoda <rhodaivioje@gmail.com>
2022-09-30 15:47:24 +0100

    finished profile sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/nav/Nav';
+import Sidebar from './components/profile page/classroom-profile-elements/sidebar/sidebar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+        <Sidebar />
     </div>
   );
 }
---
+++ src/components/profile page/classroom-profile-elements/sidebar/sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import me from '../../../../static/assets/me.jpg';
+import './sidebar.css';
+import {
+  CameraAltOutlined,
+  PersonOutline,
+  ChevronRightOutlined,
+  SchoolOutlined,
+  Check,
+  LogoutOutlined
+} from '@mui/icons-material';
+
+const Sidebar = () => {
+  return (
+    <div className='profile_container'>
+      <div className='img'>
+        <img src={me} alt='Rhoda' />
+        <div className='img_icon'><CameraAltOutlined className='img_icon-i' /></div>
+      </div>
+      <p className='username'>Eboreime Rhoda</p>
+
+      <div className='links'>
+        <div className='profile_link'>
+          <span><PersonOutline /></span>
+          <p>Profile</p>
+          <i><ChevronRightOutlined /></i>
+        </div>
+        <div className='courses_link'>
+          <span><SchoolOutlined /></span>
+          <p>Courses</p>
+          <i><ChevronRightOutlined /></i>
+        </div>
+        <div className='attendance_link'>
+          <span><Check /></span>
+          <p>Attendance</p>
+          <i><ChevronRightOutlined /></i>
+        </div>
+        <div className='logout'>
+          <i><LogoutOutlined /></i>
+          <p>Logout</p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
